30a562b6a1982b2c4a281e437ab4bdabb03f1505
abhijit-banerjee <71992427+abhijit-banerjee@users.noreply.github.com>
2022-07-12 15:16:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+function preload(){
+   clownnose = loadImage(i.postimg.cc/V63tWFFM/colwn-nose.png);
+}
+noseX=0;
+noseY = 0;
+function setup(){
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size = (300, 300);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded(){
+    console.log ("PoseNet is iniatilized");
+}
+function draw(){
+    image(video, 0, 0 , 300 , 300);
+  fill(255 ,0 , 0);
+    stroke(255, 0, 0);
+    circle(noseX, noseY, 20)
+    image(clownnose, noseX, noseY, 30 ,30);
+}
+function take_snapshot(){
+    save('MyFilteredImage.png');
+}
+function gotPoses(results){
+    if (results.length >0){
+        console.log(results);
+        noseX = results[0].pose.nose.x -15;
+        noseY=  results[0].pose.nose.y- 15;
+        console.log("nose x =" + noseX);
+        console.log ("nose y =" + noseY);
+    }
+}
